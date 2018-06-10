2e59037558f076de53248cda6b5e328312e3389b
SirKreker <il.egorov-2011@yandex.ru>
2018-06-10 19:41:16 +0300

    move buttons

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QDebug>
+using namespace std;
+
+int NUMG=1;
+int QuesNUM=4;
+QString strNUMG=QString::number(NUMG);
+QString strQuesNUM=QString::number(QuesNUM);
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -8,6 +14,7 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     ui->BackButton->setEnabled(false);
+    ui->label->setText(strNUMG + " из " + strQuesNUM);
 }
 
 MainWindow::~MainWindow()
@@ -17,12 +24,28 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_BackButton_clicked()
 {
-
+    if (NUMG-1==1){
+        ui->BackButton->setEnabled(false);
+    }
+    else {
+        ui->NextButton->setEnabled(true);
+    }
+    NUMG--;
+    strNUMG=QString::number(NUMG);
+    ui->label->setText(strNUMG + " из " + strQuesNUM);
 }
 
 void MainWindow::on_NextButton_clicked()
 {
-
+    if (NUMG+1==QuesNUM){
+        ui->NextButton->setEnabled(false);
+    }
+    else {
+        ui->BackButton->setEnabled(true);
+    }
+    NUMG++;
+    strNUMG=QString::number(NUMG);
+    ui->label->setText(strNUMG + " из " + strQuesNUM);
 }
 
 void MainWindow::on_EndButton_clicked()
