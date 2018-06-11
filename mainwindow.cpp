ba321da366b7144dd6101efe3b0a35318d6d6182
SirKreker <il.egorov-2011@yandex.ru>
2018-06-11 14:00:15 +0300

    changes numbers of answers

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QDebug>
+#include <QFileDialog>
 using namespace std;
 
 int NUMG=1;
 int QuesNUM=4;
 QString strNUMG=QString::number(NUMG);
 QString strQuesNUM=QString::number(QuesNUM);
+
+//QString str5 = QFileDialog::getOpenFileName(0, "Open Dialog", "", "*.xml");
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -31,9 +34,32 @@ void MainWindow::on_BackButton_clicked()
     else {
         ui->NextButton->setEnabled(true);
     }
+
     NUMG--;
     strNUMG=QString::number(NUMG);
     ui->label->setText(strNUMG + " из " + strQuesNUM);
+
+    //QString str = ui->NumVarSpinBox->text();
+    //int k = str.toInt();
+    int k=4; //количество вариантов ответа
+    QLayoutItem *child;
+    while((child = ui->AnswerLayout->takeAt(0))!=0)
+    {
+        delete child->widget();
+        delete child;
+    }
+    for(int i=1;i<=k;i++)
+    {
+        QCheckBox *checkBox = new QCheckBox(this);
+        QLineEdit *lineEdit = new QLineEdit(this);
+        checkBox->setText(QString::number(i)+")");
+        lineEdit->setReadOnly(true);
+        ui->AnswerLayout->addWidget(checkBox);
+        ui->AnswerLayout->addWidget(lineEdit);
+        //Line.push_back(lineEdit);
+        //Check.push_back(checkBox);
+    }
+
 }
 
 void MainWindow::on_NextButton_clicked()
@@ -45,9 +71,32 @@ void MainWindow::on_NextButton_clicked()
     else {
         ui->BackButton->setEnabled(true);
     }
+
     NUMG++;
     strNUMG=QString::number(NUMG);
     ui->label->setText(strNUMG + " из " + strQuesNUM);
+
+    //QString str = ui->NumVarSpinBox->text();
+    //int k = str.toInt();
+    int k=8; //количество вариантов ответа
+    QLayoutItem *child;
+    while((child = ui->AnswerLayout->takeAt(0))!=0)
+    {
+        delete child->widget();
+        delete child;
+    }
+    for(int i=1;i<=k;i++)
+    {
+        QCheckBox *checkBox = new QCheckBox(this);
+        QLineEdit *lineEdit = new QLineEdit(this);
+        checkBox->setText(QString::number(i)+")");
+        lineEdit->setReadOnly(true);
+        ui->AnswerLayout->addWidget(checkBox);
+        ui->AnswerLayout->addWidget(lineEdit);
+        //Line.push_back(lineEdit);
+        //Check.push_back(checkBox);
+    }
+
 }
 
 void MainWindow::on_EndButton_clicked()
